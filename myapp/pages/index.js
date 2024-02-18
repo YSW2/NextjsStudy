afe60baea412e544e469a92ccfd35d9faffac7b1
YSW2 <jrgyber@gmail.com>
2024-02-19 03:34:00 +0900

    jwt commit

--- myapp/pages/index.js
+++ myapp/pages/index.js
@@ -4,6 +4,7 @@ import { serverUrl } from "./utils/server";
 
 export default function Home() {
   const [userName, setUserName] = useState("");
+  const [isLogin, setIsLogin] = useState(false);
 
   const getUserInfo = async (access_token) => {
     try {
@@ -18,17 +19,46 @@ export default function Home() {
     }
   };
 
-  useEffect(() => {
-    getUserInfo(localStorage.getItem("access_token")).then((data) => {
-      setUserName(data.userName);
-    });
-  });
+  const logout = () => {
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("refresh_token");
+    setUserName("");
+    setIsLogin(false);
+  };
 
-  return (
-    <div>
-      <h4 className="title">쿠팡후레시</h4>
-      <p className="title-sub">by {userName}</p>
-      <Link href="/login">로그인</Link>
-    </div>
-  );
+  useEffect(() => {
+    const access_token = localStorage.getItem("access_token");
+    const refresh_token = localStorage.getItem("refresh_token");
+    if (refresh_token) {
+      setIsLogin(true);
+      getUserInfo(access_token).then((data) => {
+        if (data && data.userName) {
+          setUserName(data.userName);
+        } else {
+          // 토큰이 유효하지 않은 경우 로그아웃 처리
+          logout();
+        }
+      });
+    }
+  }, []);
+
+  function online() {
+    return (
+      <div>
+        <h1>Hi, {userName}!</h1>
+        <button onClick={logout}>로그아웃</button>
+      </div>
+    );
+  }
+
+  function offline() {
+    return (
+      <div>
+        <h1>로그인 해주세요</h1>
+        <Link href="/login">로그인</Link>
+      </div>
+    );
+  }
+
+  return <div>{isLogin ? online() : offline()}</div>;
 }
