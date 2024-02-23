610a0d905b3057842f39fa3d27c7aa2724a655a1
YSW2 <jrgyber@gmail.com>
2024-02-23 22:39:43 +0900

    post serializer commit

---
+++ backend/board/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Post
+from django.contrib.auth import get_user_model
+
+User = get_user_model
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
+    class Meta:
+        model = Post
+        fields = "__all__"
+
+    def validate(self, attrs):
+        title = attrs.get("title")
+        content = attrs.get("content")
+
+        if not (len(title) > 0 and len(content) > 0):
+            msg = "제목 및 글 내용 오류"
+            raise serializers.ValidationError(msg, code="content")
+
+    def create(self, validated_data):
+        post = Post.objects.create(**validated_data)
+
+        return post
